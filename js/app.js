1695f8e067f36b567ba1c55e29f554ba98d95852
CAH1983 <carolineho1983@yahoo.fr>
2018-09-02 11:00:35 +0100

    Updated project

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 $(() => {
-  const characters = ['{','}','[', ']', '<', '>', '/', '*', '#', '%^*', '@'];
+  const characters = ['{','}','[', ']', '<', '>', '/', '*', '#', '@'];
   const $squares = $('div.box');
-  let timer = 0;
+  const $displayTime = $('aside.timer');
+  const charactersDisplayed = [];
+  let timerId = 0;
   let score = 0;
+  let currentTime = 60;
 
 
-  // ~~~~~ function to insert a random character inside a  random square ~~~
+  // ~~~~~ Function to display a random character inside a  random square, store this character inside an array~~~
 
   function insertChar () {
     // generate a random square index
@@ -19,15 +22,48 @@ $(() => {
     // select the index of $characters
     const randomChar = characters[randomCharIdx];
     // display the character
-    return $randomSq.text(randomChar);
+    $randomSq.text(randomChar);
+    // store the character into an array
+    charactersDisplayed.push(randomChar);
   }
-
   insertChar();
-// ~~~~~~~~~~~~ 
+  // ~~~~~~~~~~~~ a random character will appear every second in a square ~~~~~~~~~~~~
   setInterval(insertChar, 1000);
 
+  // ~~~~~~~~~~~~~~ create a function to score points ~~~~~~~~~~~~~
+  $(document).on('keyup', (e) => {
+    if (charactersDisplayed.includes(e.key)) score++;
+    console.log('SCORE ==========>', score);
+    score.text(score);
+    //remove the character typed from the array characterDisplayed
+    // ??? characterDisplayed.??(e.key.value); ???
 
-// ~~~~~~~~~~~~~~ create a function to score points ~~~~~~~~~~~~~
+    const i = charactersDisplayed.indexOf(randomChar);
+    if (keyPressed = randomChar) {
+      charactersDisplayed.splice(i,)
+    }
 
-// ~~~~~~~~~~~~~~ create a timer set up to 60s ~~~~~~~~~~~~~~~~~
+  }
+  );
+
+  // ~~~~~~~~~~~~~~ create a timer set up to 60s ~~~~~~~~~~~~~~~~~
+  function timerCount() {
+    timerId = setInterval(()=>{
+      currentTime--;
+      $displayTime.text(currentTime);
+      if(currentTime === 0) {
+        clearInterval(timerId);
+      }
+    }, 1000);
+    console.log('Current time ======>', currentTime);
+  }
+
+  timerCount();
+  function speak (message) {
+    var msg = new SpeechSynthesisUtterance(message);
+    var voices = window.speechSynthesis.getVoices();
+    msg.voice = voices[0];
+    window.speechSynthesis.speak(msg);
+  }
+  speak('Help the turtle to code, type as fast as you can, each correct answer scores 1 point, each combo scores 5');
 });
